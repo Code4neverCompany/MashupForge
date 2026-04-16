b44174481b8e44ab70900ed7d31a9f507a01ccbf
4neverCompany <4nevercompany@gmail.com>
2026-04-16 06:56:58 +0200

    chore: add increment-version.sh to repo

---
+++ scripts/increment-version.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# Auto-increment version for each build
+
+cd "$(dirname "$0")/.."
+
+# Read current version from package.json
+current=$(grep '"version"' package.json | sed 's/.*"version": "\(.*\)".*/\1/')
+echo "Current version: $current"
+
+# Parse version parts
+major=$(echo $current | cut -d. -f1)
+minor=$(echo $current | cut -d. -f2)
+patch=$(echo $current | cut -d. -f3)
+
+# Increment patch
+patch=$((patch + 1))
+new_version="$major.$minor.$patch"
+
+echo "New version: $new_version"
+
+# Update package.json
+sed -i "s/\"version\": \"$current\"/\"version\": \"$new_version\"/" package.json
+
+# Update tauri.conf.json
+sed -i "s/\"version\": \"$current\"/\"version\": \"$new_version\"/" src-tauri/tauri.conf.json
+
+echo "Version bumped to $new_version"
+
+# Commit
+git add package.json src-tauri/tauri.conf.json
+git commit -m "chore: bump version to $new_version" 2>&1 | tail -3
